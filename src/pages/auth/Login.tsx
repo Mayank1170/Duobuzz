4f3eced943a2dd5040f153bd555d1e1d7ffbb554
Mayank1170 <agarwalmayank1170@gmail.com>
2024-11-06 01:50:04 +0530

    Some changes in Login

--- src/pages/auth/Login.tsx
+++ src/pages/auth/Login.tsx
@@ -3,7 +3,7 @@ import { loginEndpoint } from "../../spotify";
 const Login = () => {
   return (
     <div>
-      <a href={loginEndpoint}>
+      <a href={loginEndpoint} className="h-[100vh]  w-full flex items-center justify-center">
         <div>Login with Spotify</div>
       </a>
     </div>
